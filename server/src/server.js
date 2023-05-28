606715159e1792f451954eb9c94c6509aafa43f4
Lucrada <iamdracula.dracula@gmail.com>
2023-05-28 18:21:16 +0530

    refactored server.js

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,6 @@ app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use("/cms", require("./routes/cms.route.js"))
 
 app.get("/", (req, res) => {
