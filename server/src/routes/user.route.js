312d955725153ee6870150f6c82ec7e9037dd738
Lucrada <iamdracula.dracula@gmail.com>
2023-06-01 19:20:37 +0530

    edited delete/ to delete

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -10,6 +10,6 @@ router.post('/login', validateUserToken, loginController);
 router.post('/register', validateUserToken, registerController);
 router.post('/logout', validateUserToken, logoutController);
 
-router.delete('/delete/', validateUserToken, deleteController);
+router.delete('/delete', validateUserToken, deleteController);
 
 module.exports = router;
